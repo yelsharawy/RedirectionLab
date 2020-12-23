cccde381a2653c5d8aa5547ba70ffe306449d81e
yelsharawy <yelsharawy20@stuy.edu>
2020-12-23 13:13:07 -0500

    Commented out old approach, replaced with Scanner approach
    there are only spaces between each word and newlines between each line;
    no space at the end of the line or newline at the end of the output

--- MakeStars.java
+++ MakeStars.java
@@ -1,9 +1,33 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class MakeStars {
 
     public static void main(String[] args) {
-        
+        /*
+        try {
+            int num;
+            while ((num = System.in.read()) != -1) {
+                char c = (char)num;
+                System.out.print(Character.isWhitespace(c) ? c : '*');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+        Scanner inputScan = new Scanner(System.in);
+        while (inputScan.hasNextLine()) {
+            String line = inputScan.nextLine();
+            Scanner lineScan = new Scanner(line);
+            while (lineScan.hasNext()) {
+                String word = lineScan.next();
+                for (int i = 0; i < word.length(); i++) {
+                    System.out.print('*');
+                }
+                if (lineScan.hasNext()) System.out.print(' ');
+            }
+            if (inputScan.hasNextLine()) System.out.println();
+        }
     }
 
 }
