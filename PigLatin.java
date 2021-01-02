70766e28fde38b4890e81dc25010bece5f9f7c06
yelsharawy <yelsharawy20@stuy.edu>
2021-01-01 20:54:41 -0500

    Implemented pigLatin

--- PigLatin.java
+++ PigLatin.java
@@ -11,4 +11,34 @@ public class PigLatin {
         }
     }
 
+    private static String[] diagraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+
+    private static boolean startsWithDiagraph(String s) {
+        if (s.length() < 2) return false;
+        String firstTwo = s.substring(0, 2);
+        // i considered using another switch statement here
+        // but i didn't want a block of ugly code
+        // for minimal, at best questionable, speed improvements
+        for (String diagraph : diagraphs) {
+            if (firstTwo.equals(diagraph)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String pigLatin(String word) {
+        word = word.toLowerCase();
+        char firstChar = word.charAt(0);
+
+        switch (firstChar) {
+            case 'a': case 'e': case 'i': case 'o': case 'u':
+                return word + "hay";
+        }
+
+        return startsWithDiagraph(word) ?
+                word.substring(2) + word.substring(0, 2) + "ay" :
+                word.substring(1) + firstChar + "ay";
+    }
+    
 }
